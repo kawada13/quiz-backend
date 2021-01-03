56874a3419892a2df57f7e9b51b2a65ccaf59348
kawada <vgnsl94@gmail.com>
2021-01-03 21:38:45 +0900

    first commit

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::truncate();
+
+        $categories = [
+            [
+                'name' => 'プログラミング',
+                'thumbnail' => 'programming.png',
+                'sort' => 1
+            ],
+            [
+                'name' => '音楽',
+                'thumbnail' => 'music.png',
+                'sort' => 2
+            ],
+            [
+                'name' => '歴史',
+                'thumbnail' => 'history.png',
+                'sort' => 3
+            ],
+            [
+                'name' => 'ビジネス',
+                'thumbnail' => 'business.png',
+                'sort' => 4
+            ],
+        ];
+
+        foreach($categories as $categoy) {
+            Category::create($categoy);
+        }
+    }
+}
